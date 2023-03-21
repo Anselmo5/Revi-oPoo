a5a89d9bc72e4a93e0228adb39a6b41b3a96170a
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-21 18:27:23 -0300

    Update Pri1.js

--- ReviçãoPoo.js/Pri1.js
+++ ReviçãoPoo.js/Pri1.js
@@ -237,8 +237,9 @@
         this.arma = arma;
     }
 
-    function Humano(nome){
+    function Humano(nome,){
         this.nome = nome;
+    
     }
 
     let android = new Robo('Xyz', 'Punhos');
