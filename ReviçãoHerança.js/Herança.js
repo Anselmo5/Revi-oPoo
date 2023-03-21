235b010f7c863c383aabde939b08115a6a55b84f
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-21 20:28:02 -0300

    Update Herança.js

--- ReviçãoHerança.js/Herança.js
+++ ReviçãoHerança.js/Herança.js
@@ -2,14 +2,17 @@
 // A declaração é bem parecida com constructor functins;
 // As propriedades devem ficar num método especial chamado constructor
 
-class Tenis {
-    constructor(modelo,cor){
-        this.modelo = modelo;
-        this.cor = cor;
-    }
-}
+// class Tenis {
+//     constructor(modelo,cor){
+//         this.modelo = modelo;
+//         this.cor = cor;
+//     }
+// }
 
-console.log(typeof Tenis);
-let allstar = new Tenis("All Star", "Branco");
+// console.log(typeof Tenis);
+// let allstar = new Tenis("All Star", "Branco");
 
 
+
+// Herança
+// Utilizando a palavra extends, uma classe herda as propriedades e métodos de outra;
