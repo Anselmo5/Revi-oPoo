4f9d5f03c94591190a4a31f233fe95a6a109eb81
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-21 20:31:28 -0300

    Update Herança.js

--- ReviçãoHerança.js/Herança.js
+++ ReviçãoHerança.js/Herança.js
@@ -16,3 +16,19 @@
 
 // Herança
 // Utilizando a palavra extends, uma classe herda as propriedades e métodos de outra;
+ class Animal{
+    constructor(nome){
+        this.nome = nome;
+    }
+ }
+
+ class Cachorro extends  Animal{
+    latir(){
+        console.log("Au au" );
+    }
+ }
+
+ let bob = new Cachorro("Bob");
+ bob.latir();
+
+ console.log(bob.nome);
