2195529ed86a07797f9765191f3a22a3da58a1c9
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-21 20:23:12 -0300

    Update Herança.js

---
+++ ReviçãoHerança.js/Herança.js
@@ -0,0 +1,13 @@
+// Relenbrando (definindo classes)
+// A declaração é bem parecida com constructor functins;
+// As propriedades devem ficar num método especial chamado constructor
+
+class Tenis {
+    constructor(modelo,cor){
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+}
+
+console.log(typeof Tenis);
+let allstar = new Tenis("All Star", "Branco")
