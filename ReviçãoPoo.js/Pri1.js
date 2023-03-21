0ceb832dae2d798a554a4aa21bd612e543895e81
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-21 18:22:19 -0300

    Update Pri1.js

--- ReviçãoPoo.js/Pri1.js
+++ ReviçãoPoo.js/Pri1.js
@@ -230,4 +230,21 @@
 // console.log(y.constructor);
 // console.log(z.constructor);
 
-//
+//O operador instanceof
+// uma maneita de saber de qual instância (pai) vem algum objeto, alem de ser mais pratico que usar o constructor
+    function Robo(nome, arma){
+        this.nome = nome;
+        this.arma = arma;
+    }
+
+    function Humano(nome){
+        this.nome = nome;
+    }
+
+    let android = new Robo('Xyz', 'Punhos');
+
+    console.log(android instanceof Robo);
+    console.log( android instanceof Humano);
+
+
+
