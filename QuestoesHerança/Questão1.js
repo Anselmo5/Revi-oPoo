d0088963b53db0a89f7024fed05443e1d6d3f35b
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-22 11:27:05 -0300

    Update Questão1.js

---
+++ QuestoesHerança/Questão1.js
@@ -0,0 +1,20 @@
+class Animal {
+    constructor(nome,idade,cor){
+        this.nome = nome,
+        this.idade = idade
+        this.cor = cor
+    }
+
+     Correr(){
+        console.log('Corre bil'); // simplemente imprime
+     }
+
+     CorrerXkm(km){
+        console.log('Correr Bil' +km+ 'km/h');  // chama e imprime o valor     }
+     }
+     DistanciaMax(){
+        return '100km'  //sempre ira retornar o valor
+     }
+}  // instancia (padrão)
+
+
