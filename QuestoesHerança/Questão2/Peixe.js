c6e2fd73325ce66565a394bbda712bab2a6526c5
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-22 12:19:17 -0300

    Update Peixe.js

--- QuestoesHerança/Questão2/Peixe.js
+++ QuestoesHerança/Questão2/Peixe.js
@@ -1,5 +1,11 @@
 class peixe extends Animal{
-    constructor(){
-        
+    constructor(nome,cor,ambiente,comprimento,velocidade,patas,caracteristicas){
+        super(nome,cor,ambiente,comprimento,velocidade,patas,)
     }
+
+    Dados(obj){
+        console.log('Nome' + this.nome +'\nAmbiente' + this.ambiente
+        + '\nCaracteristica')+this.cararacteristica;
+    }
+
 }
