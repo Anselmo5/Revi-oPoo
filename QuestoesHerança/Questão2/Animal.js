d13a8bcaac01be6423c9f03e6d4d9a167d23e358
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-22 12:12:24 -0300

    Create Animal.js

---
+++ QuestoesHerança/Questão2/Animal.js
@@ -0,0 +1,40 @@
+class Animal {
+    #nome // priventi
+    constructor(nome,cor,ambiente,comprimento,velocidade,patas){
+    this.#nome =nome,
+    this.cor =cor,
+    this.ambiente =ambiente,
+    this.comprimento=comprimento,
+    this.velocidade=velocidade,
+    this.patas=patas
+    }
+
+    alternome(nome){
+        this.nome = nome
+    }
+    
+    alterCor(cor){
+        this.cor = cor
+    }
+    
+    alterambiente(ambiente){
+        this.ambiente =ambiente
+    }
+
+    
+    altercomprimento(comprimento){
+        this.comprimento =comprimento
+    }
+    
+    altervelocidade(velocidade){
+        this.velocidade = velocidade
+    }
+
+    alterpatas(patas){
+        this.patas = patas
+    }
+    
+    Dados(obj){
+        console.table(bjt);
+    }
+}
