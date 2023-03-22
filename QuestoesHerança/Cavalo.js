34519ce2388d56fb8ba8293a8a0884fb46e3d827
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-22 11:42:04 -0300

    Create Cavalo.js

---
+++ QuestoesHerança/Cavalo.js
@@ -0,0 +1,15 @@
+
+class Cavalo extends Animal {
+    constructor(nome,idade,cor,raça){
+        super(nome,idade,cor) 
+        // Quem crior o constructor que crie
+        this.raça = raça // esse parametor so existe no cavalo então não precisar passar no super porque raça não pertence a animal
+
+    }
+
+    Correr(nome){{
+        console.log('Corre bil'+nome);
+    }}
+}  // instancia (padrão)
+
+
