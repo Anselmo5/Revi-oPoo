d1fb177f0338e1458812e3a9aecc12d2daf25039
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-21 22:12:30 -0300

    Update Questão1.js

---
+++ QuestoesHerança/Questão1.js
@@ -0,0 +1,19 @@
+let Produto ={
+    nomeProduto(){
+        window.prompt('QUAL É O NOME DO PRODUTO: ')
+    },
+
+    PreçodoProduto(){
+        window.prompt('Qual foi o preço do seu produto')
+    },
+
+    EssafoisuaOrdem(){
+        window.confirm('Essa foi sua ordem'=+this.nomeProduto+this.PreçodoProduto)
+    }
+}
+
+
+let Dados ={
+
+}
+
