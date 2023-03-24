69361093efc2cbea1207968863cbd4c229f4c318
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-24 10:00:48 -0300

    Update Parte2.js

--- QuestãesTreinamento/Parte2.js
+++ QuestãesTreinamento/Parte2.js
@@ -1,9 +1,10 @@
 class Pessoa2 extends Pessoa {
-    constructor(nome,idade,falar,empresa,entrevistado){
+    constructor(nome,idade,falar,empresa,entrevistado,empregar){
     super(nome,idade,falar)
     this.empresa = empresa
     this.entrevistado = entrevistado
     this.empregado = false
+    this,empregar = empregar
     
     }
 
@@ -11,10 +12,17 @@ class Pessoa2 extends Pessoa {
         console.log('Ola '+this.entrevistado.nome,' Me chamo Anselmo e estou aqui para conduzi sua entrevista ');
     }
 
+    VerificarStatus(){
+        if (this.empregado=== true) {
+            return 'Aprovado'
+          } else {
+           return 'Dessemprecado'
+          }
+    }
+
     Empresa(){
         console.log(this.entrevistado.nome,'Você Foi emprecado');
         this.empregado = true
-        return this.empregado
     }
 
 }
