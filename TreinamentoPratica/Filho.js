e5e5606a697b8c9d02b9fb92075ddcc58a4977aa
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-28 20:48:45 -0300

    Create Filho.js

---
+++ TreinamentoPratica/Filho.js
@@ -0,0 +1,9 @@
+// class filho extends pai{
+//     constructor(nome,idade,aniversario,filhodequem){
+//         super(nome,idade,aniversario)
+//         this.filhodequem = filhodequem
+//     }
+
+
+
+// }
